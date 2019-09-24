0ff36d7d80b29e9dc8275a4bd4a32ff2cd20dfcb
Partizan_LERO <corazonsergey@gmail.com>
2019-09-24 12:46:40 +0200

    add functions for searching addresses

--- update_schema_house.sql
+++ update_schema_house.sql
@@ -5,7 +5,7 @@ BEGIN;
     alter table house rename column houseguid to houseguid_x;
     alter table house add column houseguid uuid;
     update house set houseguid = houseguid_x::uuid;
-    alter table addrobj drop column houseguid_x;
+    alter table house drop column houseguid_x;
 COMMIT;
 
 -- cast column house.houseid to uuid
@@ -13,7 +13,7 @@ BEGIN;
     alter table house rename column houseid to houseid_x;
     alter table house add column houseid uuid;
     update house set houseid = houseid_x::uuid;
-    alter table addrobj drop column houseid_x;
+    alter table house drop column houseid_x;
 COMMIT;
 
 -- cast column house.aoguid to uuid
@@ -21,7 +21,7 @@ BEGIN;
     alter table house rename column aoguid to aoguid_x;
     alter table house add column aoguid uuid;
     update house set aoguid = aoguid_x::uuid;
-    alter table addrobj drop column aoguid_x;
+    alter table house drop column aoguid_x;
 COMMIT;
 
 
