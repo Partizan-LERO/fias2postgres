0990996dd155755a476ceec07f6d75c0e816e7d7
Partizan_LERO <corazonsergey@gmail.com>
2019-09-26 12:01:21 +0200

    change index type

--- house_indexes.sql
+++ house_indexes.sql
@@ -4,8 +4,8 @@ CREATE EXTENSION pg_trgm;
 --========== HOUSE ==========--
 
 --  create btree indexes
-CREATE UNIQUE INDEX aoguid_pk_idx ON house USING btree (aoguid);
-CREATE UNIQUE INDEX houseid_idx ON house USING btree (houseid);
-CREATE UNIQUE INDEX houseguid_idx ON house USING btree (houseguid);
+CREATE INDEX aoguid_pk_idx ON house USING btree (aoguid);
+CREATE INDEX houseid_idx ON house USING btree (houseid);
+CREATE INDEX houseguid_idx ON house USING btree (houseguid);
 CREATE INDEX housenum_idx ON house USING btree (housenum);
 
