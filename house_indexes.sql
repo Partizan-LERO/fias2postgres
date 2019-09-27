58c2cc04578da92b3362663d162c52748d71a49e
Partizan_LERO <corazonsergey@gmail.com>
2019-09-27 09:23:24 +0200

    modify indexes

--- house_indexes.sql
+++ house_indexes.sql
@@ -1,11 +1,8 @@
--- extension to implement trigrams;
-CREATE EXTENSION pg_trgm;
-
 --========== HOUSE ==========--
 
 --  create btree indexes
 CREATE INDEX aoguid_pk_idx ON house USING btree (aoguid);
 CREATE INDEX houseid_idx ON house USING btree (houseid);
 CREATE INDEX houseguid_idx ON house USING btree (houseguid);
-CREATE INDEX housenum_idx ON house USING btree (housenum);
+--CREATE INDEX housenum_idx ON house USING btree (housenum);
 
